cb3a644393491ada0e68ab6d05d3ca4bff85ef75
kchan2 <ka.chan@us.fujitsu.com>
2015-11-04 18:11:02 -0600

    modify add_record for datetime object

--- add_record.py
+++ add_record.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from models import Tweet
+import time
+from time import mktime
+from datetime import datetime
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///idb.db'
 db = SQLAlchemy(app)
 
-# data = Tweet(2, "9527", "some text", "random guy", "www.tweetcity.me2", 30.30, -127.27, 5)
-# db.session.add(data)
-# db.session.commit()
-
+data = Tweet("9527", "some text", "random guy", "www.tweetcity.abc", datetime.fromtimestamp(mktime(time.strptime("Mon Nov 02 15:01:54 2015", "%a %b %d %H:%M:%S %Y"))) ,30.30, -127.27, 5)
+db.session.add(data)
+db.session.commit()
+print(db.session.query(Tweet).first().id)
 
 db.session.query(Tweet).delete()
 db.session.commit()

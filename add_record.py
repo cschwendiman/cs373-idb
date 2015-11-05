a01e3081eb77d391ea4438e9d9025751dd1a8034
masteret <masteret9432@gmail.com>
2015-11-05 01:14:33 -0600

    finally got a working script to push data

--- add_record.py
+++ add_record.py
@@ -6,42 +6,47 @@ import time
 from time import mktime
 from datetime import datetime
 import json
+from sqlalchemy import exists
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///idb.db'
 db = SQLAlchemy(app)
 
-db.drop_all()
+db.session.query(Tweet).delete()
+db.session.query(Hashtag).delete()
+db.session.query(Location).delete()
+db.session.commit()
+
 db.configure_mappers()
 db.create_all()
-
-# db.session.query(Tweet).delete()
-# db.session.commit()
 
 # for many to many adding
 # Senpai notice me!
 # http://docs.sqlalchemy.org/en/latest/orm/tutorial.html#building-a-many-to-many-relationship
 
-tweets = json.load(open("../cs373-tweetCity/new_AUX.json"))
-for tweet_id, info in tweets.items():
-    data = Tweet(tweet_id, info["text"], info["name"], "http://twitter.com/statuses/"+tweet_id,\
-    datetime.fromtimestamp(mktime(time.strptime(info["datetime"].replace("+0000", ""), "%a %b %d %H:%M:%S %Y"))), \
-    info["geo"]["coordinates"][0], info["geo"]["coordinates"][1], info["location_id"])
-    db.session.add(data)
+cities = [Location("Austin", "Texas", "United States"), Location("San Francisco", "California", "United States"), Location("New York City", "New York", "United States")]
+for city in cities:
+    db.session.add(city)
 
-    for hashy in info["hashtags"]:
-        if db.session.query(Hashtag).filter(Hashtag.text == hashy).all():
-            data = Hashtag(hashy, "http://twitter.com/statuses/"+tweet_id)
-            db.session.add(data)
+hashed = []
 
-# db.session.commit()
-# print(db.session.query(Tweet).all())
+for index, path in enumerate(["../cs373-tweetCity/new_AUX.json", "../cs373-tweetCity/new_SF.json", "../cs373-tweetCity/new_NYC.json"]):
+    tweets = json.load(open(path))
+    for tweet_id, info in tweets.items():
+        data = Tweet(tweet_id, info["text"], info["name"], "https://twitter.com/statuses/"+tweet_id,\
+        datetime.fromtimestamp(mktime(time.strptime(info["datetime"].replace("+0000", ""), "%a %b %d %H:%M:%S %Y"))), \
+        info["geo"]["coordinates"][0], info["geo"]["coordinates"][1], info["location_id"])
+        data.location = cities[index]
+        db.session.add(data)
+        cur_tweet = data    
 
-# data = Tweet("9527", "some text", "random guy", "www.tweetcity.abc", datetime.fromtimestamp(mktime(time.strptime("Mon Nov 02 15:01:54 2015", "%a %b %d %H:%M:%S %Y"))) ,30.30, -127.27, 5)
-# db.session.add(data)
-# db.session.commit()
-# print(db.session.query(Tweet).first().id)
+        for hashy in info["hashtags"]:
+            if hashy not in hashed:
+                hashed.append(hashy)
+                data = Hashtag(hashy, "https://twitter.com/hashtag/"+hashy)
+                data.tweets.append(cur_tweet)
+                db.session.add(data)
+                cities[index].hashtags.append(data)
 
-# db.session.query(Tweet).delete()
-# db.session.commit()
+db.session.commit()

81ebdf434f91b4cbd7562900fc81ee8570dc6e12
masteret <kaheichan@utexas.edu>
2015-11-05 12:31:29 -0600

    typo in tweets api call

--- __init__.py
+++ __init__.py
@@ -19,14 +19,14 @@ db = SQLAlchemy(app)
 # API Requests
 @app.route("/api/tweets/")
 def tweets():
-    raw_data = db.session.query(Location).all()
+    raw_data = db.session.query(Tweet).all()
     json_dict = {}
     for data in raw_data:
         data = data.__dict__
         del data['_sa_instance_state']
         data["date_time"] = data["date_time"].strftime("%Y-%m-%d %H:%M:%S")
         json_dict[data["id"]] = data
-    return json.dumps(json_dict, ensure_ascii=False)
+    return json.dumps(json_dict, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
 
 @app.route("/api/tweet/<int:id>/")
 def tweet(id):
@@ -34,7 +34,7 @@ def tweet(id):
     data = data.__dict__
     del data['_sa_instance_state']
     data["date_time"] = data["date_time"].strftime("%Y-%m-%d %H:%M:%S")
-    return json.dumps(data, ensure_ascii=False)
+    return json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
 
 @app.route("/api/hashtags/")
 def hashtags():
@@ -44,14 +44,14 @@ def hashtags():
         data = data.__dict__
         del data['_sa_instance_state']
         json_dict[data["id"]] = data
-    return json.dumps(json_dict, ensure_ascii=False)
+    return json.dumps(json_dict, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
 
 @app.route("/api/hashtag/<int:id>/")
 def hashtag(id):
     data = db.session.query(Hashtag).filter_by(id=id).first()
     data = data.__dict__
     del data['_sa_instance_state']
-    return json.dumps(data, ensure_ascii=False)
+    return json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
 
 @app.route("/api/locations/")
 def locations():
@@ -61,14 +61,15 @@ def locations():
         data = data.__dict__
         del data['_sa_instance_state']
         json_dict[data["id"]] = data
-    return json.dumps(json_dict, ensure_ascii=False)
+    return json.dumps(json_dict, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
+
 
 @app.route("/api/location/<int:id>/")
 def location(id):
     data = db.session.query(Location).filter_by(id=id).first()
     data = data.__dict__
     del data['_sa_instance_state']
-    return json.dumps(data, ensure_ascii=False)
+    return json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
 
 # Funnel all other requests to angular
 @app.route('/', defaults={'path': ''})

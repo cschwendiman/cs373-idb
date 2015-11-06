b17b7b6633d559a10e95658766b9248893d1cf86
masteret <kaheichan@utexas.edu>
2015-11-05 20:11:29 -0600

    create a new branch

--- tests.py
+++ tests.py
@@ -22,10 +22,6 @@ class testModels(TestCase):
 
     #setup the database
     def setUp(self):
-        app = Flask(__name__, static_url_path='/static')
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test_idb.db'
-        db = SQLAlchemy(app)
         db.configure_mappers()
         db.create_all()
 
